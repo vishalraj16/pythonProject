f681a428b50d201a35ebd6d6d16423f4ada0d994
vishalraj16 <jivishalraj@gmail.com>
2025-02-18 07:58:46 +0530

    Initial commit

---
+++ practice project.py
@@ -0,0 +1,65 @@
+# self introduction of myself by using variables
+
+# types of variable
+
+#string variable
+name = "vishal raj"
+
+# integer variable
+age = 19
+
+# float variable
+height = 5.4
+
+#string variable
+study = "b.com"
+college_name = "B.D College, Patna"
+address = "gardanibagh"
+
+# boolean variable
+is_student = True
+
+#variables ko print karna
+print ("Mera naam hai:", name)
+print ("Mera age hai:", age)
+print ("Meri height hai:", height)
+print ("Main study karta hoon", study)
+print ("Mera college ka naam hai", college_name)
+print ("Mera address hai", address)
+print ("kya main ek student hoon?", is_student)
+
+# Arithmetic Operation using Python variables
+
+# Description: Is code mein basic arithmetic operations  (addition, subtraction, multiplication,
+# division, integer division, modulus) ka use kiya gya hai
+
+#variables define
+a = 8
+b = 3
+
+# Arithmetic operations
+addition = a + b                 # Addition
+subtraction = a - b              # Subtraction
+multiplication = a * b           # Multiplication
+division = a / b                 # Floating-point division
+int_division = a // b            # Integer division
+modulus = a % b                  # Modulus remainder
+
+#print
+
+print("Variables: a=", a, "aur b =" , b)
+print("Addition (a + b):",addition)
+print("Subtraction (a - b):", subtraction)
+print("Multiplication (a * b):", multiplication)
+print("division (a / b):", division)
+print("Integer Division (a // b):", int_division)
+print("Modulus (a % b):", modulus)
+
+#over
+
+
+
+
+
+
+
